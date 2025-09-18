8b8bfa1442628b9bad179f413d52bc8bff387100
kingjukster <99943086+kingjukster@users.noreply.github.com>
2025-09-18 13:09:32 -0500

    set up react and website basics

---
+++ MC_Tunes/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import MCT from './assets/mc_tunes_logo.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://github.com/kingjukster/MC_Tunes" target="_blank">
+          <img src={MCT} className="logo logo-large" alt="MC Tunes logo" />
+        </a>
+      </div>
+      <h1>MC Tunes</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
